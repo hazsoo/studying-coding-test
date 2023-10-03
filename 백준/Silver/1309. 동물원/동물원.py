2303484b335b2a4d91eb034bb84ea86eef75e1f3
Jisoo Ha <81146582+hazsoo@users.noreply.github.com>
2023-10-03 20:48:11 +0900

    Update 동물원.py

--- 백준/Silver/1309. 동물원/동물원.py
+++ 백준/Silver/1309. 동물원/동물원.py
@@ -7,7 +7,7 @@ for i in range(1, n+1):
     d[i][0] = d[i-1][0] + d[i-1][1] + d[i-1][2]
     d[i][1] = d[i-1][0] + d[i-1][2]
     d[i][2] = d[i-1][0] + d[i-1][1]
-    for j in range(3):
+    for j in range(3): # 이 부분 없으면 '메모리초과' 됨
         d[i][j] %= 9901
 
 print(sum(d[n]) % 9901)
