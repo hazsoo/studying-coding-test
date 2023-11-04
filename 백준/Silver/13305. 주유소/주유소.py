249c14fd59a0f697692f96bb37daa0c4fff98cf3
Jisoo Ha <81146582+hazsoo@users.noreply.github.com>
2023-11-04 22:35:25 +0900

    [Silver III] Title: 주유소, Time: 116 ms, Memory: 46236 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -1,15 +1,12 @@
 import sys
 input = sys.stdin.readline
 n = int(input())
-r = list(map(int, input().split()))
-p = list(map(int, input().split()))
-ans = p[0]*r[0]
-i = 1
-while i <= n-2:
-    if p[i] <= p[i+1]:
-        ans += p[i]*(r[i] + r[i+1])
-        i += 2
-    elif p[i] > p[i+1]:
-        ans += p[i]*r[i]
-        i += 1
+km = list(map(int, input().split()))
+price = list(map(int, input().split()))
+min_price = price[0]
+ans = 0
+for i in range(n-1):
+    if min_price > price[i]:
+        min_price = price[i]
+    ans += min_price*km[i]
 print(ans)
