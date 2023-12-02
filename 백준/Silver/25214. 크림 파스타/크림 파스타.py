459dbf01bfc4a37c8ec5e949590ac7d97849a692
Jisoo Ha <81146582+hazsoo@users.noreply.github.com>
2023-12-02 22:21:04 +0900

    [Silver IV] Title: 크림 파스타, Time: 304 ms, Memory: 53244 KB -BaekjoonHub

---
+++ 백준/Silver/25214. 크림 파스타/크림 파스타.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+a = list(map(int, input().split()))
+dp = [0]*n
+min_value = a[0]
+for i in range(1, n):
+    min_value = min(min_value, a[i])
+    dp[i] = max(dp[i-1], a[i] - min_value)
+print(*dp)
