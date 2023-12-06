e318e6642a4ff7c536059bdd394d249df3e2cb34
Jisoo Ha <81146582+hazsoo@users.noreply.github.com>
2023-12-06 22:22:10 +0900

    [Silver III] Title: 피보나치 함수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+dp = [[0]*2 for _ in range(41)]
+dp[0] = [1,0]
+dp[1] = [0,1]
+dp[2] = [1,1]
+for i in range(3, 41):
+    dp[i] = [dp[i-2][0]+dp[i-1][0], dp[i-2][1]+dp[i-1][1]]
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    print(*dp[n])
