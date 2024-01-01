cae7537ca4982a9fb108da151c247ec81f877b49
Jisoo Ha <81146582+hazsoo@users.noreply.github.com>
2024-01-01 23:38:27 +0900

    [Silver II] Title: 상자넣기, Time: 164 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1965. 상자넣기/상자넣기.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+box = list(map(int, input().split()))
+dp = [0]*(n+1)
+for i in range(n):
+    dp[i] = 1
+    for j in range(i):
+        if box[i] > box[j] and dp[j]+1 > dp[i]:
+            dp[i] = dp[j]+1
+print(max(dp))
