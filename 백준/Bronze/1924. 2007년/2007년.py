295a83baf95337e893a9b2b53c7de443d9496d85
Jisoo Ha <81146582+hazsoo@users.noreply.github.com>
2024-01-11 19:55:56 +0900

    [Bronze I] Title: 2007년, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1924. 2007년/2007년.py
+++ 백준/Bronze/1924. 2007년/2007년.py
@@ -1,13 +1,12 @@
 import sys
 input = sys.stdin.readline
 x, y = map(int, input().split())
+
 month = [31,28,31,30,31,30,31,31,30,31,30,31]
 day = ['SUN','MON','TUE','WED','THU','FRI','SAT']
+
 days = 0
-if x == 1:
-    days += y
-else:
-    for i in range(x-1):
-        days += month[i]
-    days += y
+for i in range(x-1):
+    days += month[i]
+days += y
 print(day[days%7])
