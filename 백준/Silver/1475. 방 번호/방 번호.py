d0a9e46897f685b07b3c5a03a655da13c7fc6e6b
Jisoo Ha <81146582+hazsoo@users.noreply.github.com>
2024-01-14 17:58:24 +0900

    [Silver V] Title: 방 번호, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.py
+++ 백준/Silver/1475. 방 번호/방 번호.py
@@ -1,16 +1,15 @@
 import sys
 input = sys.stdin.readline
-n = int(input())
-nlist = list(map(int,str(n)))
+n = input().rstrip()
 set = [0]*10
 
-for i in range(len(nlist)):
-    if nlist[i] == 6 or nlist[i] == 9:
+for i in n:
+    if i == '6' or i == '9':
         if set[6] < set[9]:
             set[6] += 1
         else:
             set[9] += 1
     else:
-        set[nlist[i]] += 1
+        set[int(i)] += 1
 
 print(max(set))
