b46da8b37812db3ddabbeebd050174b17acafa16
Jisoo Ha <81146582+hazsoo@users.noreply.github.com>
2024-01-15 23:58:54 +0900

    [Silver V] Title: 너의 평점은, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.py
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -1,31 +1,16 @@
 import sys
 input = sys.stdin.readline
 
+grade = ['A+','A0','B+','B0','C+','C0','D+','D0','F']
+score = [4.5, 4.0, 3.5, 3.0, 2.5, 2.0, 1.5, 1.0, 0]
+
 sum_score = 0
-sum_grade = 0
+sum_point = 0
 for _ in range(20):
-    grade = input().split()
-    grade[1] = float(grade[1])
-    if grade[2] != 'P':
-        sum_grade += grade[1]
-    if grade[2] == 'A+':
-        sum_score += grade[1] * 4.5
-    elif grade[2] == 'A0':
-        sum_score += grade[1] * 4.0
-    elif grade[2] == 'B+':
-        sum_score += grade[1] * 3.5
-    elif grade[2] == 'B0':
-        sum_score += grade[1] * 3.0
-    elif grade[2] == 'C+':
-        sum_score += grade[1] * 2.5
-    elif grade[2] == 'C0':
-        sum_score += grade[1] * 2.0
-    elif grade[2] == 'D+':
-        sum_score += grade[1] * 1.5
-    elif grade[2] == 'D0':
-        sum_score += grade[1] * 1.0
-    elif grade[2] == 'F':
-        sum_score += grade[1] * 0.0
-    else:
-        continue
-print('{:.6f}'.format(sum_score/sum_grade))
+    s, p, g = input().split()
+    p = float(p)
+    if g != 'P':
+        sum_point += p
+        sum_score += p * score[grade.index(g)]
+
+print('{:.6f}'.format(sum_score/sum_point))
