cfbab2d730a57876d5a068590d80bb5cc8570fa1
Jisoo Ha <81146582+hazsoo@users.noreply.github.com>
2024-01-17 23:58:46 +0900

    [Silver IV] Title: 에라토스테네스의 체, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+arr = [True]*(n+1)
+cnt = 0
+
+for i in range(2, n+1):
+    for j in range(i, n+1, i):
+        if arr[j] != False:
+            arr[j] = False
+            cnt += 1
+            if cnt == k:
+                print(j)
