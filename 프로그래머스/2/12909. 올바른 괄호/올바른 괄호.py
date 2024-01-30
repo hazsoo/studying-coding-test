cb0427ce37bbb5120ab60e7b5da662a884456390
Jisoo Ha <81146582+hazsoo@users.noreply.github.com>
2024-01-30 22:56:37 +0900

    [level 2] Title: 올바른 괄호, Time: 6.56 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,14 @@
+def solution(s):
+    answer = False
+    stk = []
+    for i in s:
+        if i == '(':
+            stk.append(1)
+        else:
+            try:
+                stk.pop()
+            except:
+                return answer
+    if not stk:
+        answer = True
+    return answer
