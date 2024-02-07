a2735ffabd18cdb9455ffc6f5609377d87791f09
Jisoo Ha <81146582+hazsoo@users.noreply.github.com>
2024-02-07 23:16:13 +0900

    [Gold IV] Title: 테트로미노, Time: 4604 ms, Memory: 37456 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.py
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -1,7 +1,8 @@
+# 테트로미노
 blocks = (
     ((0,1), (0,2), (0,3)),
     ((1,0), (2,0), (3,0)),
-    ((1,0), (1,1), (1,2)), 
+    ((1,0), (1,1), (1,2)),
     ((0,1), (1,0), (2,0)),
     ((0,1), (0,2), (1,2)),
     ((1,0), (2,0), (2,-1)),
@@ -19,21 +20,24 @@ blocks = (
     ((1,0), (2,0), (1,1)),
     ((1,0), (2,0), (1,-1))
 )
+
 n, m = map(int, input().split())
 a = [list(map(int, input().split())) for _ in range(n)]
-ans = 0
+maxsum = 0
+
 for i in range(n):
     for j in range(m):
-        for block in blocks:
+        for b in blocks:
             ok = True
             s = a[i][j]
-            for dx, dy in block:
+            for dx, dy in b:
                 x, y = i+dx, j+dy
                 if 0 <= x < n and 0 <= y < m:
                     s += a[x][y]
                 else:
                     ok = False
                     break
-            if ok and ans < s:
-                ans = s
-print(ans)
+            if ok and maxsum < s:
+                maxsum = s
+
+print(maxsum)
