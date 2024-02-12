6f2396f0e98ea38eba691eec482df5b57402dbb9
Jisoo Ha <81146582+hazsoo@users.noreply.github.com>
2024-02-12 20:16:02 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 50.40 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,17 +1,12 @@
-from collections import defaultdict
-
 def solution(participant, completion):
     answer = ''
-    pd = defaultdict(lambda: 0)
-    for p in participant:
-        pd[p] += 1
-    pd = dict(pd)
-    
-    for c in completion:
-        pd[c] -= 1
-        if pd[c] == 0:
-            pd.pop(c)
-        
-    for key in pd.keys():
-        answer += key
+    temp = 0
+    dic = {}
+    for part in participant:
+        dic[hash(part)] = part
+        temp += int(hash(part))
+    for com in completion:
+        temp -= hash(com)
+    answer = dic[temp]
+
     return answer
