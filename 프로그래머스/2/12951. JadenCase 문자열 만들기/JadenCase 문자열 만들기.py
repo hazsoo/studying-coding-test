0647664725bd562eb4c75f632e3cff4e8fa10beb
Jisoo Ha <81146582+hazsoo@users.noreply.github.com>
2024-02-18 23:41:38 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -0,0 +1,12 @@
+def solution(s):
+    s = list(s)
+    for i in range(len(s)):
+        if s[i] == ' ':
+            continue
+        if i == 0:
+            s[i] = s[i].upper()
+        elif s[i-1] == ' ':
+            s[i] = s[i].upper()
+        else:
+            s[i] = s[i].lower()
+    return ''.join(s)
