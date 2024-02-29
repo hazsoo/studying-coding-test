e3e30f5bb1f7082ba8f23463e6acaf7da0b54185
Jisoo Ha <81146582+hazsoo@users.noreply.github.com>
2024-02-29 22:32:29 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 254.65 ms, Memory: 43.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -0,0 +1,12 @@
+def solution(elements):
+    l = len(elements)
+    sum_set = set()
+    
+    for i in range(l):
+        e = elements[i]
+        sum_set.add(e)
+        for j in range(i+1, i+l):
+            e += elements[j%l]
+            sum_set.add(e)
+        
+    return len(sum_set)
