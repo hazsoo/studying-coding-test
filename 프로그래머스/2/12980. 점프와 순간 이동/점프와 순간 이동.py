f9b041fa9c03f7b09ab74b918a53de120356258c
Jisoo Ha <81146582+hazsoo@users.noreply.github.com>
2024-03-17 00:23:05 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
@@ -1,9 +1,10 @@
 def solution(n):
-    ans = 1
+    answer = 1
     while n >= 2:
         if n % 2 == 0:
             n /= 2
         else:
             n //= 2
-            ans += 1
-    return ans
+            answer += 1
+        
+    return answer
