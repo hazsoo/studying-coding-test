bc4fe3a964a98c6c72069f9574d204c0570daa69
Jisoo Ha <81146582+hazsoo@users.noreply.github.com>
2024-03-28 14:07:42 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,26 +1,27 @@
 def solution(s):
     cnt = 0
     s_pairs = {
-        ']':'[',
+        ')':'(',
         '}':'{',
-        ')':'('
+        ']':'['
     }
     
     for i in range(len(s)):
         st = []
         rot = s[i:] + s[:i]
         
-        for j in rot:
-            if j in ')}]':
-                if st and st[-1] == s_pairs[j]:
+        for r in rot:
+            if r in ')}]':
+                if st and st[-1] == s_pairs[r]:
                     st.pop(-1)
                     continue
                 else:
                     break
-            st.append(j)
+            st.append(r)
         
         else:
             if not st:
                 cnt += 1
-                
+            
     return cnt
+            
