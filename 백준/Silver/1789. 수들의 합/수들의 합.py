00dc72d9a64f9c0cc2f707e137d7273e5ea2035e
Jisoo Ha <81146582+hazsoo@users.noreply.github.com>
2024-04-18 01:25:40 +0900

    [Silver V] Title: 수들의 합, Time: 64 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1789. 수들의 합/수들의 합.py
+++ 백준/Silver/1789. 수들의 합/수들의 합.py
@@ -1,12 +1,11 @@
-import sys
-input = sys.stdin.readline
 s = int(input())
-i = 0
 n = 0
+i = 0
+
 while True:
     if s > i:
         i += 1
-        s = s-i
+        s = s - i
         n += 1
     else:
         print(n)
