8f63c298db859df43bbb53c2f96f32c5fc5c6487
Jisoo Ha <81146582+hazsoo@users.noreply.github.com>
2024-04-22 23:49:09 +0900

    [Silver IV] Title: 괄호, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,20 +1,21 @@
 t = int(input())
+
 for _ in range(t):
     ps = input()
-    vps = "YES"
     st = []
+    vps = "YES"
 
     for i in range(len(ps)):
         if ps[i] == "(":
-            st.append(ps[i])
+            st.append("(")
         else:
             try:
                 st.pop()
             except:
                 vps = "NO"
                 break
-        
+
     if st:
         vps = "NO"
-    
+
     print(vps)
