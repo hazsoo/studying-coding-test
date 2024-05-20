017f99353a611df0abcca2d8d80e2e7f45d41c51
Jisoo Ha <81146582+hazsoo@users.noreply.github.com>
2024-05-20 21:33:38 +0900

    Create N과 M （1）_2.py

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）_2.py
@@ -0,0 +1,18 @@
+import sys
+
+input = sys.stdin.readline
+n, m = map(int, input().split())
+arr = []
+
+def bt():
+    if len(arr) == m:
+        print(*arr)
+        return
+    
+    for i in range(1, n+1):
+        if i not in arr:
+            arr.append(i)
+            bt()
+            arr.pop()
+            
+bt()
